42b706f349f822f4c66b73c1200c04d05b9bd0b6
Matthew Hanson <hansons.matt@gmail.com>
2023-04-15 17:03:48 -0700

    MySQL integration, dryrun, config file

--- fidelityTransactionsToFinance.py
+++ fidelityTransactionsToFinance.py
@@ -2,7 +2,9 @@ import getopt
 import argparse
 import csv
 import datetime
+import uuid
 import mysql.connector
+import configparser
 
 def validateBuy(transactionDict):
     validationErrors = []
@@ -362,8 +364,11 @@ parser.add_argument("-i", "--input", type=open, required=True, help="Transaction
 parser.add_argument("-o", "--output", type=str, required=True, help="Transaction output csv file")
 parser.add_argument("-e", "--error", type=str, default="error.csv", help="Path to error output file")
 parser.add_argument("-a", "--account", type=str, required=True, help="SMA or Brokerage")
+parser.add_argument("-c", "--config", type=str, default="config.ini", help="Path to configuration file")
+parser.add_argument("-d", "--dryrun", type=bool, default=True, help="Dry run only, no DB inserts (default to dry run)")
 # Read arguments from command line
 args = parser.parse_args()
+print("Program arguments = " + str(args))
 
 smaAccount = 'Y80057518'
 smatransactionTypeMap = {
@@ -433,7 +438,7 @@ fieldnames = ['brokerage_account_number',
 'symbol',
 'name',
 'source_shares',
-'currency','source_price_per_share','source_fees','source_commission','source_transaction_amount',
+'currency','source_price_per_share','source_fees','source_commissions','source_transaction_amount',
 'normalized_shares','normalized_transaction_amount','transaction_notes'
 ]
 outputFile = open(args.output, 'w', newline='')
@@ -448,16 +453,40 @@ errorfieldnames = ['brokerage_account_number',
 'symbol',
 'name',
 'source_shares',
-'currency','source_price_per_share','source_fees','source_commission','source_transaction_amount',
+'currency','source_price_per_share','source_fees','source_commissions','source_transaction_amount',
 'normalized_shares','normalized_transaction_amount','transaction_notes','errors'
 ]
 errorFile = open(args.error, 'w', newline='')
 errorWriter = csv.DictWriter(errorFile, fieldnames=errorfieldnames)
 errorWriter.writeheader()
 
+config = configparser.ConfigParser()
+config.read(args.config)
+mySqlHost = config['mysql-config']['host']
+mySqlUser = config['mysql-config']['user']
+mySqlPwd = config['mysql-config']['password']
+print("Config parameters = " + str(config))
+# mysql
+mysqlConnection = mysql.connector.connect(
+  host=mySqlHost,
+  user=mySqlUser,
+  password=mySqlPwd
+)
+print(mysqlConnection)
+cursor = mysqlConnection.cursor()
+#insertStatement = ("insert into finance.transactions_staging "
+#                   "(brokerage_account_number,transaction_id,transaction_date,transaction_type,transaction_desc,symbol,name,source_shares,currency,source_price_per_share,source_fees,source_commission,source_transaction_amount,normalized_shares,normalized_transaction_amount,transaction_notes) "
+#                   "values (%(brokerage_account_number)s,%(transaction_id)s,%(transaction_date)s,%(transaction_type)s,%(transaction_desc)s,%(symbol)s,%(name)s,%(source_shares)s,%(currency)s,%(source_price_per_share)s,%(source_fees)s,%(source_commission)s,%(source_transaction_amount)s,%(normalized_shares)s,%(normalized_transaction_amount)s,%(transaction_notes)s)"
+#)
+insertStatement = ("insert into finance.transactions_staging "
+                   "(brokerage_account_number,transaction_id,transaction_date,transaction_type,transaction_desc,symbol,name,source_shares,source_price_per_share,source_fees,source_commissions,source_transaction_amount) "
+                   "values (%(brokerage_account_number)s,%(transaction_id)s,%(transaction_date)s,%(transaction_type)s,%(transaction_desc)s,%(symbol)s,%(name)s,%(source_shares)s,%(source_price_per_share)s,%(source_fees)s,%(source_commissions)s,%(source_transaction_amount)s)"
+)
+
 numRows = 0
 numProcessed = 0
 numErrors = 0
+numInserted = 0
 reader = csv.DictReader(args.input)
 for row in reader:
         numRows += 1
@@ -485,7 +514,9 @@ for row in reader:
             else:
                 errorsList.append("MULTIPLE TRANSACTION TYPE MATCHES")
 
+        #theUUID = uuid.uuid4()
         transactionDict = {'brokerage_account_number' : accountNumber,
+                'transaction_id' : str(uuid.uuid4()),
                 'transaction_date': runDate,
                 'transaction_desc' : row['Action'].strip(),
                 'transaction_type' : transactionType,
@@ -494,7 +525,7 @@ for row in reader:
                 'source_shares' : row['Quantity'],
                 'source_price_per_share' : row[columnNameMap['priceColName']],
                 'source_fees' : row[columnNameMap['sourceFeesColName']],
-                'source_commission' : row[columnNameMap['sourceCommissionColName']],
+                'source_commissions' : row[columnNameMap['sourceCommissionColName']],
                 'source_transaction_amount' : row[columnNameMap['sourceTransactionAmountColName']]}
 
 
@@ -512,9 +543,64 @@ for row in reader:
             transactionDict['errors'] = errorString
             rowWriter = errorWriter
 
+        if (valid):
+            # insert
+            try:
+                symbol = row['Symbol'].strip() if row['Symbol'].strip() else None
+                sourceShares = float(row['Quantity']) if row['Quantity'] else None
+                sourcePricePerShare = float(row[columnNameMap['priceColName']]) if row[columnNameMap['priceColName']] else None
+                sourceFees = float(row[columnNameMap['sourceFeesColName']]) if row[columnNameMap['sourceFeesColName']] else None
+                sourceCommissions = float(row[columnNameMap['sourceCommissionColName']]) if row[columnNameMap['sourceCommissionColName']] else None
+                sourceTransactionAmt = float(row[columnNameMap['sourceTransactionAmountColName']]) if row[columnNameMap['sourceTransactionAmountColName']] else None
+
+                insertTransactionDict = {'brokerage_account_number' : accountNumber,
+                    'transaction_id' : str(uuid.uuid4()),
+                    'transaction_date': runDate,
+                    'transaction_desc' : row['Action'].strip(),
+                    'transaction_type' : transactionType,
+                    #'symbol' : row['Symbol'].strip(),
+                    'symbol' : symbol,
+                    'name' : row['Security Description'].strip(),
+                    #'source_shares' : row['Quantity'],
+                    'source_shares' : sourceShares,
+                    #'source_price_per_share' : row[columnNameMap['priceColName']],
+                    'source_price_per_share' : sourcePricePerShare,
+                    #'source_fees' : row[columnNameMap['sourceFeesColName']],
+                    'source_fees' : sourceFees,
+                    #'source_commissions' : row[columnNameMap['sourceCommissionColName']],
+                    'source_commissions' : sourceCommissions,
+                    #'source_transaction_amount' : row[columnNameMap['sourceTransactionAmountColName']]
+                    'source_transaction_amount' : sourceTransactionAmt
+                    }
+                
+                print("inserting " + str(insertTransactionDict))
+                cursor.execute(insertStatement, insertTransactionDict)
+                numInserted += 1
+                print("MySQL table insert SUCCESSFUl!!!")
+            except mysql.connector.Error as error:
+                print("Unable to insert " + str(insertTransactionDict))
+                print("Insert error" + str(error))
+                numErrors += 1
+                transactionDict['errors'] = str(error)
+                rowWriter = errorWriter
+
         rowWriter.writerow(transactionDict)
 
 print("Found '" + str(numRows) + "' rows in input file. Processed '" + str(numProcessed) + "' rows, encountered '" + str(numErrors) + "' errors")
 
 outputFile.close()
 errorFile.close()
+
+if (args.dryrun):
+    print("This is a dryrun, rolling back inserts")
+    mysqlConnection.rollback()
+else:
+    if (numInserted == numRows):
+        print("All good, committing inserts")
+        mysqlConnection.commit()
+    else:
+        print("Rows inserted '" + str(numInserted) + "' not equal to rows processed '" + str(numRows)+ "'. Rolling back inserts")
+        mysqlConnection.rollback()
+
+cursor.close()
+mysqlConnection.close()
